008f72003596183ed61db6d7d65d790a054cea8e
akshaykp709 <akshaykp709@gmail.com>
2024-08-06 14:55:19 +0530

    Update main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+namespace servos {
+    //% block="servo P0" fixedInstance whenUsed
+    export const P0 = new servos.PinServo(pins.P0);
+    //% block="servo P1" fixedInstance whenUsed
+    export const P1 = new servos.PinServo(pins.P1);
+    //% block="servo P2" fixedInstance whenUsed
+    export const P2 = new servos.PinServo(pins.P2);
+}
